a0b80afc8d3ecc5f4f7fe4774506b5199a33118c
Ramón Guijarro <hola@soyguijarro.com>
2016-08-15 13:45:42 +0200

    Fix Rotten Tomatoes Reviews Redirect script not working for https

--- Rotten_Tomatoes_Reviews_Redirect.user.js
+++ Rotten_Tomatoes_Reviews_Redirect.user.js
@@ -8,9 +8,9 @@
 // @updateURL   https://raw.githubusercontent.com/soyguijarro/userscripts/master/Rotten_Tomatoes_Reviews_Redirect.user.js
 // @icon        https://raw.githubusercontent.com/soyguijarro/userscripts/master/img/rotten_tomatoes_icon.png
 // @license     GPLv3; http://www.gnu.org/licenses/gpl.html
-// @version     1.0
-// @include     http://www.rottentomatoes.com/m/*
-// @include     http://www.rottentomatoes.com/m/*/reviews/*
+// @version     1.1
+// @include     *://www.rottentomatoes.com/m/*
+// @include     *://www.rottentomatoes.com/m/*/reviews/*
 // @run-at      document-start
 // @grant       none
 // ==/UserScript==
